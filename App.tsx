b9fa2a64e54f51abedf83dcf43dcde0c00d5132e
Kubra Ergin <kubraaergiin@gmail.com>
2025-08-01 00:55:23 +0300

    Second commit

--- App.tsx
+++ App.tsx
@@ -1,27 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootNavigator } from "./src/navigations/RootNavigator";
 import Toast from "react-native-toast-message";
-import WelcomePrompts from "./src/components/WelcomePrompts";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="WelcomePrompts">
-            {(props) => (
-              <WelcomePrompts
-                {...props}
-                onSelectPrompt={(prompt) => {
-                  console.log("Test prompt seçildi:", prompt);
-                }}
-              />
-            )}
-          </Stack.Screen>
-        </Stack.Navigator>
+        <RootNavigator />
       </NavigationContainer>
       <Toast />
     </>
