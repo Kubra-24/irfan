427141517ddf168dfb99e67b659af1973c6e0572
Kubra-24 <kubraaergiin@gmail.com>
2025-10-03 00:35:58 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://txisyzhqemzxzumxywxr.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4aXN5emhxZW16eHp1bXh5d3hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNjA1NzIsImV4cCI6MjA2OTYzNjU3Mn0.rdLV_D3rowm0Pd8rrQBxeFJIhO4UjhpQg19Ni_V4lTQ";
+const SUPABASE_URL = "";
+const SUPABASE_PUBLISHABLE_KEY = "";
 export const supabase = createClient<Database>(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY,
